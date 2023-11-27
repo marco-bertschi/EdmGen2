ababa72ab46e4c7ea6503774cea39c969094f682
marco-bertschi <bertschi@outlook.com>
2023-11-27 15:42:01 +0100

    Assembly Version updated

--- src/EdmGen2/Properties/AssemblyInfo.cs
+++ src/EdmGen2/Properties/AssemblyInfo.cs
@@ -6,13 +6,14 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EdmGen2")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("EdmGen2, modified by Marco Bertschi")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("EdmGen2")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyMetadata("Repository", "https://github.com/marco-bertschi/EdmGen2")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
